58d921d4bd4e0da939ccd6fef78c638a318426a6
anton-chornobai <antonchornobajj1@gmail.com>
2025-08-02 21:28:13 +0200

    t4

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -5,7 +5,6 @@ import java.util.Random;
 public class Lottery {
     private final ColorSupplier supplier = new ColorSupplier();
 
-
     public Ball getRandomBall() {
         Colors newColor = supplier.getRandomColor();
         int number = new Random().nextInt(100);
