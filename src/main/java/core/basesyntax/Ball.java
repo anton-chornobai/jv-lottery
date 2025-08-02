c6ab4c5ff795a097987fbdc8fefa84784bbb8d82
anton-chornobai <antonchornobajj1@gmail.com>
2025-08-02 21:33:52 +0200

    t5

--- src/main/java/core/basesyntax/Ball.java
+++ src/main/java/core/basesyntax/Ball.java
@@ -4,30 +4,13 @@ public class Ball {
     private Colors color;
     private int number;
 
-    int getNumber() {
-        return this.number;
-    }
-
-    void setNumber(int num) {
-        this.number = num;
-    }
-
-    Colors getColor() {
-        return this.color;
-    }
-
-    void setColor(Colors color) {
-        this.color = color;
-    }
-
-
-    Ball(Colors color, int number) {
+    public Ball(Colors color, int number) {
         this.color = color;
         this.number = number;
     }
 
     @Override
     public String toString() {
-        return "My color is " + color + " My number is " + number;
+        return "Ball [color=" + color + ", number=" + number + "]";
     }
 }
